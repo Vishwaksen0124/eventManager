a3de28f292284b8861047bb34691a5328bbfbcaf
Vishwaksen0124 <vishwaksenpujala@gmail.com>
2025-02-09 11:21:56 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://vishwaksenpujala:DPXwAaBvCfUQCFpF@cluster0.02rif.mongodb.net/', {
+mongoose.connect('mongodb+srv://vishwaksenpujala:DPXwAaBvCfUQCFpF@cluster0.02rif.mongodb.net/EventManager', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 }).then(() => {
